0e42235597278496297c70f2ae069c8193a00ae4
AlejandroJAA <alexxela012@gmail.com>
2021-06-21 20:17:22 +0200

    first commit

---
+++ ClassCliente.php
@@ -0,0 +1,87 @@
+<?php
+
+class Cliente {
+    //Estado
+    private $dni;
+    private $nombre;
+    private $apellidos;
+    private $fnac;
+    private $email;
+    private $dir
+
+    //Comportamiento
+
+    function __construct($dni,$nombre,$apellidos,$fnac,$email,$dir) {
+        $this->dni = $dni;
+        $this->nombre = $nombre;
+        $this->apellidos = $apellidos;
+        $this->fnacimiento = $fnac;
+        $this->email = $email;
+        $this->direccion = $dir
+
+        
+    }
+
+    //darse de alta
+    function darAlta($conn) {
+        // Consulta para realizar inserción a la base de datos
+
+        $sql = "INSERT INTO Clientes (dni,nombre,apellidos,fnacimiento,email,direccion) VALUES ('".$this->dni."','".$this->nombre."','".$this->apellidos."','".$this->fnacimiento."','".$this->email."','".$this->direccion."');";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+            //hago la construccion del email y lo mando
+            $miEmail = new envioEmail($this->email);
+            $miEmail->sendMail();
+        
+    
+          } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+          }
+
+
+    }
+
+    function asignarEmail($nuevoEmail) {
+      $this->email = $nuevoEmail;
+    }
+    
+    //Buscar un/unos cliente/s dentro de la BBDD y mostrarlo por pantalla
+    function buscar($busqueda,$tipoBusqueda,$conn) {
+
+        // Consulta para realizar la búsqueda en la base de datos
+        $sql = "SELECT * FROM Clientes WHERE ";
+        switch ($tipoBusqueda){
+        case "onom":
+          $sql = $sql."nombre like '%$busqueda%';";
+        break;
+        case "oape":
+          $sql = $sql."apellidos like '%$busqueda%';";
+        break;
+        case "omail":
+          $sql = $sql."email like '%$busqueda%';";
+        break;
+        case "odni":
+          $sql = $sql."dni like '%$busqueda%';";
+        break;
+        default:
+          echo "Se ha producido un error durante la búsqueda.";
+      }
+
+      $resultado = $conn->query($sql);
+
+      // Consulta para realizar la busqueda en la base de datos
+      if ($resultado->num_rows > 0) {
+        // Salida de datos por cada fila
+        while($row = $resultado->fetch_assoc()) {
+          echo "- Nombre: ".$row["nombre"].", Apellidos: ".$row["apellidos"].", Email: ".$row["email"].", DNI: ".$row["dni"].", Dirección: ".$row["direccion"]."<br>";
+        }
+      }else{
+        echo "No se han encontrado resultados.";
+      }
+    }
+
+
+   }
+
+?>
---
+++ insertarClientes.php
@@ -0,0 +1,68 @@
+<?php
+
+require './ClassCliente.php';
+
+// Variables 
+$servername = "localhost";
+$username = "php";
+$password = "1234";
+$dbname = "pruebas";
+$dni = $_POST["fdni"];
+$fnom = $_POST["fnom"];
+$fape = $_POST["fape"];
+$fmail = $_POST["fmail"];
+$fdate = $_POST["fdate"];
+$direccion = $_POST["direccion"];
+
+compruebaEmail();
+
+function compruebaEmail($fmail) {
+
+  if (!filter_var($fmail, FILTER_VALIDATE_EMAIL)) {
+
+    $erremail="email no valido";
+    
+  } else {
+
+    // Establecer conexión con la base de datos
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+  die("Error de conexión: " . $conn->connect_error);
+}
+
+//Creamos un objeto cliente y le pedimos el alta.
+
+$clienteNuevo = new Cliente($dni,$fnom,$fape,$fdate,$fmail);
+
+$clienteNuevo->darAlta($conn);
+
+
+// Cerrar la conexion a la base de datos
+$conn->close();
+
+  }
+
+}
+
+// Establecer conexión con la base de datos
+//$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+//if ($conn->connect_error) {
+//  die("Error de conexión: " . $conn->connect_error);
+//}
+
+//Creamos un objeto cliente y le pedimos el alta.
+
+//$clienteNuevo = new Cliente($dni,$fnom,$fape,$fdate,$fmail);
+
+//$clienteNuevo->darAlta($conn);
+
+
+// Cerrar la conexion a la base de datos
+//$conn->close();
+
+
+?>
